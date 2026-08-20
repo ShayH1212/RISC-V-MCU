37c720a586d346023721a23ac0f24fb43c0afff4
Shay Hinkson <shayhinkson2@gmail.com>
2026-08-20 07:49:59 -0400

    Complete single-cycle RISC-V CPU

--- testbench/cpu_tb.sv
+++ testbench/cpu_tb.sv
@@ -1,5 +1,43 @@
-module cpu_tb;
+/*
+This is the testbench for the entire simple cpu
+This tests all the necessary cases to show the CPU has passes
+The results should be as follows
 
+x1 = 5                                                                                           
+x2 = 3                                                                                           
+x3 = 2
+x4 = 1
+x5 = 7
+x6 = 40
+x7 = 5
+x8  = 6
+x9  = 1
+x10 = 0
+x11 = -16
+x12 = 2
+x13 = -4
+x14 = 42
+x15 = 42
+memory[0] = 42
+x16 = 1
+x17 = 2
+x18 = 3
+x19 = 4
+x20 = 5
+x21 = 6
+x22 = 168
+x23 = 7
+x24 = 0
+x25 = 0
+x26 = 0
+x27 = 0
+x28 = 0
+x29 = 0
+x30 = 0
+
+*/
+
+module cpu_tb;
 logic clk;
 logic reset;
 
@@ -17,44 +55,45 @@ initial begin
 
     clk = 1'b0;
     reset = 1'b1;
-
     #10;
     reset = 1'b0;
 
     // Allow CPU to execute instructions
     #500;
 
-$display("x1 = %0d", dut.register_file.registers[1]);
-$display("x2 = %0d", dut.register_file.registers[2]);
-$display("x3 = %0d", dut.register_file.registers[3]);
-$display("x4 = %0d", dut.register_file.registers[4]);
-$display("x5 = %0d", dut.register_file.registers[5]);
-$display("x6 = %0d", dut.register_file.registers[6]);
-$display("x7 = %0d", dut.register_file.registers[7]);
-$display("x8  = %0d", dut.register_file.registers[8]);
-$display("x9  = %0d", dut.register_file.registers[9]);
-$display("x10 = %0d", dut.register_file.registers[10]);
-$display("x11 = %0d", $signed(dut.register_file.registers[11]));
-$display("x12 = %0d", dut.register_file.registers[12]);
-$display("x13 = %0d", $signed(dut.register_file.registers[13]));
-$display("x14 = %0d", dut.register_file.registers[14]);
-$display("x15 = %0d", dut.register_file.registers[15]);
-$display("memory[0] = %0d", dut.data_memory.memory[0]);
-$display("x16 = %0d", dut.register_file.registers[16]);
-$display("x17 = %0d", dut.register_file.registers[17]);
-$display("x18 = %0d", dut.register_file.registers[18]);
-$display("x19 = %0d", dut.register_file.registers[19]);
-$display("x20 = %0d", dut.register_file.registers[20]);
-$display("x21 = %0d", dut.register_file.registers[21]);
-$display("x22 = %0d", dut.register_file.registers[22]);
-$display("x23 = %0d", dut.register_file.registers[23]);
-$display("x24 = %0d", dut.register_file.registers[24]);
-$display("x25 = %0d", dut.register_file.registers[25]);
-$display("x26 = %0d", dut.register_file.registers[26]);
-$display("x27 = %0d", dut.register_file.registers[27]);
-$display("x28 = %0d", dut.register_file.registers[28]);
-$display("x29 = %0d", dut.register_file.registers[29]);
-$display("x30 = %0d", dut.register_file.registers[30]);
+    // diaplay all results
+    $display("x1 = %0d", dut.register_file.registers[1]);
+    $display("x2 = %0d", dut.register_file.registers[2]);
+    $display("x3 = %0d", dut.register_file.registers[3]);
+    $display("x4 = %0d", dut.register_file.registers[4]);
+    $display("x5 = %0d", dut.register_file.registers[5]);
+    $display("x6 = %0d", dut.register_file.registers[6]);
+    $display("x7 = %0d", dut.register_file.registers[7]);
+    $display("x8  = %0d", dut.register_file.registers[8]);
+    $display("x9  = %0d", dut.register_file.registers[9]);
+    $display("x10 = %0d", dut.register_file.registers[10]);
+    $display("x11 = %0d", $signed(dut.register_file.registers[11]));
+    $display("x12 = %0d", dut.register_file.registers[12]);
+    $display("x13 = %0d", $signed(dut.register_file.registers[13]));
+    $display("x14 = %0d", dut.register_file.registers[14]);
+    $display("x15 = %0d", dut.register_file.registers[15]);
+    $display("memory[0] = %0d", dut.data_memory.memory[0]);
+    $display("x16 = %0d", dut.register_file.registers[16]);
+    $display("x17 = %0d", dut.register_file.registers[17]);
+    $display("x18 = %0d", dut.register_file.registers[18]);
+    $display("x19 = %0d", dut.register_file.registers[19]);
+    $display("x20 = %0d", dut.register_file.registers[20]);
+    $display("x21 = %0d", dut.register_file.registers[21]);
+    $display("x22 = %0d", dut.register_file.registers[22]);
+    $display("x23 = %0d", dut.register_file.registers[23]);
+    $display("x24 = %0d", dut.register_file.registers[24]);
+    $display("x25 = %0d", dut.register_file.registers[25]);
+    $display("x26 = %0d", dut.register_file.registers[26]);
+    $display("x27 = %0d", dut.register_file.registers[27]);
+    $display("x28 = %0d", dut.register_file.registers[28]);
+    $display("x29 = %0d", dut.register_file.registers[29]);
+    $display("x30 = %0d", dut.register_file.registers[30]);
+
 
     $finish;
 
